8883fd8029f19c2780d3f8aa33b6498685318435
noumi <139501014+noumidev@users.noreply.github.com>
2025-11-04 13:31:18 +0100

    Add IA and IF stage

--- src/mina_cpu/mina.sv
+++ src/mina_cpu/mina.sv
@@ -9,6 +9,7 @@
 
 import types::u32_t;
 import types::wrstb_t;
+import types::if_params_t;
 
 module mina(
     input logic clk,
@@ -24,5 +25,49 @@ module mina(
     output u32_t imem_addr,
     input  u32_t imem_data
 );
+
+    localparam INITIAL_IA = 32'b0;
+
+    // --- Instruction address register ---
+    u32_t ia;
+    u32_t ia_plus_4;
+
+    // TODO: handle branch logic
+
+    always_ff @(posedge clk) begin
+        if (!rst_n)
+            ia <= INITIAL_IA;
+        else
+            ia <= ia_plus_4;
+    end
+
+    always_comb begin
+        ia_plus_4 = ia + 32'd4;
+    end
+
+    // --- Instruction fetch ---
+    if_params_t if_params_if;
+    if_params_t if_params_id;
+
+    assign imem_addr = ia;
+
+    assign if_params_if.ia_plus_4 = ia_plus_4;
+    assign if_params_if.ir        = imem_data;
+
+    // --- IF/ID ---
+    if_id if_id0(
+        .clk(clk),
+        .rst_n(rst_n),
+        .if_params_in(if_params_if),
+        .if_params_out(if_params_id)
+    );
+
+    // --- Instruction decode ---
+
+    // --- Memory access ---
+    // TODO
+    assign dmem_addr   = '0;
+    assign dmem_wrdata = '0;
+    assign dmem_wrstb  = '0;
 
 endmodule
