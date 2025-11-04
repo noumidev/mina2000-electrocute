423e0ec54bacfb0ab4dfac63e220ced2b8cc3f87
noumi <139501014+noumidev@users.noreply.github.com>
2025-11-04 15:46:04 +0100

    Add EX definitions

--- src/defines.sv
+++ src/defines.sv
@@ -4,6 +4,9 @@ package types;
     typedef logic[ 7:0] u8_t;
     typedef logic[31:0] u32_t;
 
+    // Registers
+    typedef logic[4:0] regaddr_t;
+
     // Memory
     typedef logic[3:0] wrstb_t;
 
@@ -11,6 +14,12 @@ package types;
     typedef struct packed {
         u32_t ia_plus_4;
         u32_t ir;
-    } if_params_t;
+    } id_params_t;
+
+    typedef struct packed {
+        u32_t     op_a;
+        u32_t     op_b;
+        regaddr_t rd_addr;
+    } ex_params_t;
 
 endpackage
