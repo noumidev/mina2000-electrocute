4195eee39213de58ac9088e2e9b983505c893704
noumi <139501014+noumidev@users.noreply.github.com>
2025-11-05 10:24:53 +0100

    Simplify operand decoding

--- src/mina_cpu/id_ex.sv
+++ src/mina_cpu/id_ex.sv
@@ -7,6 +7,7 @@
  * mina_cpu/id_ex.sv - Intermediary ID/EX register
  */
 
+import types::SEL_ZERO;
 import types::ex_params_t;
 
 module id_ex(
@@ -22,9 +23,16 @@ module id_ex(
 
     always_ff @(posedge clk) begin
         if (!rst_n) begin
-            ex_params_out.op_a    <= '0;
-            ex_params_out.op_b    <= '0;
-            ex_params_out.rd_addr <= '0;
+            ex_params_out.ia_plus_4 <= '0;
+            ex_params_out.a_sel     <= SEL_ZERO;
+            ex_params_out.a_sel     <= SEL_ZERO;
+            ex_params_out.ra_addr   <= '0;
+            ex_params_out.ra_data   <= '0;
+            ex_params_out.rb_addr   <= '0;
+            ex_params_out.rb_data   <= '0;
+            ex_params_out.rd_addr   <= '0;
+            ex_params_out.imm       <= '0;
+            ex_params_out.shift     <= '0;
         end else begin
             ex_params_out <= ex_params_in;
         end
--- src/mina_cpu/id_stage.sv
+++ src/mina_cpu/id_stage.sv
@@ -9,6 +9,10 @@
 
 import types::u32_t;
 import types::regaddr_t;
+import types::sel_t;
+import types::SEL_ZERO;
+import types::SEL_IA_IMM;
+import types::SEL_REG;
 import types::id_params_t;
 import types::ex_params_t;
 
@@ -31,103 +35,67 @@ module id_stage(
     typedef logic[6:0] opcode_t;
     typedef logic[9:0] secopc_t;
 
-    typedef logic[11:0] imm_t;
-    typedef logic[19:0] uimm_t;
-    typedef logic[24:0] disp_t;
-
-    typedef struct packed {
-        opcode_t opcode;
-        secopc_t secopc;
-
-        imm_t  imm;
-        uimm_t uimm;
-        disp_t disp;
-
-        regaddr_t rd_addr;
-        regaddr_t ra_addr;
-        regaddr_t rb_addr;
-    } instr_t;
-
-    instr_t instr;
-
-    // ID <-> regfile
-    regaddr_t ra_addr;
-    regaddr_t rb_addr;
-    u32_t     ra_data;
-    u32_t     rb_data;
-
     regfile regfile0(
         .clk(clk),
         .rst_n(rst_n),
-        .ra_addr(ra_addr),
-        .ra_data(ra_data),
-        .rb_addr(rb_addr),
-        .rb_data(rb_data),
+        .ra_addr(ex_params.ra_addr),
+        .ra_data(ex_params.ra_data),
+        .rb_addr(ex_params.rb_addr),
+        .rb_data(ex_params.rb_data),
         .rd_addr(rd_addr),
         .rd_data(rd_data)
     );
 
+    opcode_t opcode;
+    secopc_t secopc;
+
+    assign opcode = id_params.ir[ 6: 0];
+    assign secopc = id_params.ir[26:17];
+
     always_comb begin
-        // Decode instruction fields
-        instr.opcode  = id_params.ir[ 6: 0];
-        instr.secopc  = id_params.ir[26:17];
-        instr.imm     = id_params.ir[31:20];
-        instr.uimm    = id_params.ir[31:12];
-        instr.disp    = id_params.ir[31: 7];
-        instr.ra_addr = id_params.ir[16:12];
-        instr.rb_addr = id_params.ir[31:27];
-        instr.rd_addr = id_params.ir[11: 7];
+        // TODO: get source from RD for stores
+        ex_params.ia_plus_4 = id_params.ia_plus_4;
+        ex_params.ra_addr   = id_params.ir[16:12];
+        ex_params.rb_addr   = id_params.ir[31:27];
+        ex_params.rd_addr   = id_params.ir[11: 7];
+
+        ex_params.imm   = '0;
+        ex_params.shift = '0;
 
         // Decode operands
-        unique case(instr.opcode) inside
+        unique case(opcode) inside
             7'b0xxxxxx: begin
                 // R-type
-                ra_addr = instr.ra_addr;
-                rb_addr = instr.rb_addr;
-
-                ex_params.op_a    = ra_data;
-                ex_params.op_b    = rb_data;
-                ex_params.rd_addr = instr.rd_addr;
+                ex_params.a_sel = SEL_REG;
+                ex_params.b_sel = SEL_REG;
             end
             7'b111110x: begin
                 // U-type
-                ra_addr = '0;
-                rb_addr = '0;
+                // opcode[0] -> MOVH, opcode[1] -> ADR
+                ex_params.a_sel = opcode[0] ? SEL_IA_IMM : SEL_ZERO;
+                ex_params.b_sel = SEL_IA_IMM;
 
-                if (instr.opcode[0]) begin
-                    // ADR
-                    ex_params.op_a = id_params.ia_plus_4;
-                end else begin
-                    // MOVH
-                    ex_params.op_a = '0;
-                end
-                
-                ex_params.op_b    = {instr.uimm, 12'b0};
-                ex_params.rd_addr = instr.rd_addr;
+                ex_params.imm = {id_params.ir[31:12], 12'b0};
             end
             7'b111111x: begin
                 // D-type
-                ra_addr = '0;
-                rb_addr = '0;
+                ex_params.a_sel = SEL_IA_IMM;
+                ex_params.b_sel = SEL_ZERO;
 
-                ex_params.op_a = '0;
-                ex_params.op_b = id_params.ia_plus_4;
+                // opcode[0] -> BRA, opcode[1] -> CALL
+                ex_params.rd_addr = opcode[0] ? 5'd31 : '0;
 
-                if (instr.opcode[0]) begin
-                    // CALL
-                    ex_params.rd_addr = 5'd31;
-                end else begin
-                    ex_params.rd_addr = '0;
-                end
+                // Special case: branch logic is handled in ID
             end
             default: begin
                 // I-type
-                ra_addr = instr.ra_addr;
-                rb_addr = '0;
+                ex_params.a_sel = SEL_REG;
+                ex_params.b_sel = SEL_IA_IMM;
 
-                ex_params.op_a    = ra_data;
-                ex_params.op_b    = {20'b0, instr.imm};
-                ex_params.rd_addr = instr.rd_addr;
+                // TODO: sign extension for some opcodes
+                ex_params.imm = {20'b0, id_params.ir[31:20]};
+
+                // TODO: shifts for loadstores
             end
         endcase
     end
