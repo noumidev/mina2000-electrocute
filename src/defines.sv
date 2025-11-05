43c6d9ffc65195b3664756f7a994428b2d55d35f
noumi <139501014+noumidev@users.noreply.github.com>
2025-11-05 10:24:39 +0100

    Add new definitions, change ex_params_t

--- src/defines.sv
+++ src/defines.sv
@@ -10,6 +10,16 @@ package types;
     // Memory
     typedef logic[3:0] wrstb_t;
 
+    // Immediate operations
+    typedef logic[1:0] shift_t;
+
+    // Operand selection
+    typedef enum logic[1:0] {
+        SEL_ZERO   = 2'b00,
+        SEL_IA_IMM = 2'b01,
+        SEL_REG    = 2'b10
+    } sel_t;
+
     // Pipeline
     typedef struct packed {
         u32_t ia_plus_4;
@@ -17,9 +27,20 @@ package types;
     } id_params_t;
 
     typedef struct packed {
-        u32_t     op_a;
-        u32_t     op_b;
+        u32_t ia_plus_4;
+
+        sel_t a_sel; // 0 -> 0, 1 ->  IA, 2 -> RA
+        sel_t b_sel; // 0 -> 0, 1 -> IMM, 2 -> RB
+
+        // Register addresses/operands
+        regaddr_t ra_addr;
+        u32_t     ra_data;
+        regaddr_t rb_addr;
+        u32_t     rb_data;
         regaddr_t rd_addr;
+
+        u32_t   imm;
+        shift_t shift;
     } ex_params_t;
 
 endpackage
