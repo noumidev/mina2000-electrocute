1d77f2ccd0de7cacbf598edb057555fa7ca705de
noumi <139501014+noumidev@users.noreply.github.com>
2025-11-05 15:40:44 +0100

    Create mem_wb.sv

---
+++ src/mina_cpu/mem_wb.sv
@@ -0,0 +1,32 @@
+/*
+ * MINA2000 "ElectroCute" is the MINAv2 reference implementation.
+ * Copyright (C) 2025  noumidev
+ */
+
+/*
+ * mina_cpu/mem_wb.sv - Intermediary MEM/WB register
+ */
+
+import types::wb_params_t;
+
+module mem_wb(
+    input logic clk,
+    input logic rst_n,
+
+    // From MEM
+    input wb_params_t wb_params_in,
+
+    // To WB
+    output wb_params_t wb_params_out
+);
+
+    always_ff @(posedge clk) begin
+        if (!rst_n) begin
+            wb_params_out.rd_addr <= '0;
+            wb_params_out.rd_data <= '0;
+        end else begin
+            wb_params_out <= wb_params_in;
+        end
+    end
+
+endmodule
